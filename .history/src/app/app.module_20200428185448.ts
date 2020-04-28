7039f914708664290faf5863fae27d6d8dbb9349
sattarwa <Vyankat.Sattarwar@tomtom.com>
2020-04-28 23:26:11 +0530

    tt csr

--- .history/src/app/app.module_20200428185448.ts
+++ .history/src/app/app.module_20200428185448.ts
@@ -4,16 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 
-import { HttpClientModule } from '@angular/common/http';
-
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
